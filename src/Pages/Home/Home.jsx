cfd26d77f9df136615db5a47446d4ae0d15b7b4e
NurahadSarker <nurahadrobi451@gmail.com>
2025-12-07 15:44:41 +0600

    added some changes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero';
 import DailyMeals from './DailyMeals';
+import Reviews from './Reviews';
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
             <DailyMeals></DailyMeals>
+            <Reviews></Reviews>
         </div>
     );
 };
